c684a825db22e1654fa3ae5a61c55b5784116e55
Dave Blencowe <dblencowe@gmail.com>
2020-02-19 02:29:49 +0000

    Adds an init command for creating a commands.yaml file

--- genie.go
+++ genie.go
@@ -48,6 +48,28 @@ func findCommandFiles() []string {
 	return discoveredFiles
 }
 
+func initCommandsFile() string {
+	path, _ := os.Getwd()
+	targetFile := path + "/commands.yaml"
+
+	_, err := os.Stat(targetFile)
+	if err == nil {
+		log.Fatalf("commands.yaml file already exists at " + path)
+		panic("commands.yaml file already exists at " + path)
+	}
+
+	t := configFile{
+		Shell: "/bin/bash",
+	}
+	content, _ := yaml.Marshal(&t)
+	err = ioutil.WriteFile(targetFile, []byte(content), 0644)
+	if err != nil {
+		log.Fatalf("Unable to create file " + path)
+	}
+
+	return path
+}
+
 func (c *configFile) getConf() *configFile {
 	files := findCommandFiles()
 	for k := range files {
@@ -69,6 +91,7 @@ func (c *configFile) getConf() *configFile {
 
 func (c *configFile) getAvailableCommands() {
 	fmt.Println("Available commands:")
+	fmt.Println("\tinit - create a commands.yaml file in the current directory\n")
 	for k := range c.Commands {
 		fmt.Println("\t" + k)
 	}
@@ -88,6 +111,12 @@ func main() {
 	}
 
 	commandName := flag.Args()[0]
+
+	if commandName == "init" {
+		path := initCommandsFile()
+		fmt.Printf("Created commands file at " + path)
+	}
+
 	for key := range c.Commands[commandName] {
 		if dryRun == true {
 			fmt.Printf(">\t%s\n", c.Commands[commandName][key].Command)
