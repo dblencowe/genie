ad56944a64ff4aed27ad8fb0cee95f250665bb0b
Dave Blencowe <dblencowe@gmail.com>
2021-12-05 17:13:46 +0000

    Clean up file

--- genie.go
+++ genie.go
@@ -1,154 +1,154 @@
 package main
 
 import (
-  "flag"
-  "fmt"
-  "io/ioutil"
-  "log"
-  "os"
-  "os/exec"
-  "path/filepath"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 
-  "github.com/imdario/mergo"
-  "github.com/mitchellh/go-homedir"
-  "gopkg.in/yaml.v2"
+	"github.com/imdario/mergo"
+	"github.com/mitchellh/go-homedir"
+	"gopkg.in/yaml.v2"
 )
 
 type environment struct {
-  Name  string `yaml:"name"`
-  Value string `yaml:"value"`
+	Name  string `yaml:"name"`
+	Value string `yaml:"value"`
 }
 
 type command struct {
-  Command     string        `yaml:"command"`
-  Environment []environment `yaml:"environment"`
+	Command     string        `yaml:"command"`
+	Environment []environment `yaml:"environment"`
 }
 
 type configFile struct {
-  Commands map[string][]command `yaml:"commands"`
-  Shell    string               `yaml:"shell"`
+	Commands map[string][]command `yaml:"commands"`
+	Shell    string               `yaml:"shell"`
 }
 
 func findCommandFiles() []string {
-  var discoveredFiles []string
-  homeDirectory, _ := homedir.Dir()
-  discoveredFiles = append(discoveredFiles, homeDirectory+"/.genie-commands.yaml")
+	var discoveredFiles []string
+	homeDirectory, _ := homedir.Dir()
+	discoveredFiles = append(discoveredFiles, homeDirectory+"/.genie-commands.yaml")
 
-  path, err := os.Getwd()
-  filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
-    if err != nil {
-      return err
-    }
-    if filepath.Base(path) == "commands.yaml" {
-      discoveredFiles = append(discoveredFiles, path)
-    }
+	path, err := os.Getwd()
+	filepath.Walk(path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if filepath.Base(path) == "commands.yaml" {
+			discoveredFiles = append(discoveredFiles, path)
+		}
 
-    return nil
-  })
-  if err != nil {
-    log.Println(err)
-  }
+		return nil
+	})
+	if err != nil {
+		log.Println(err)
+	}
 
-  return discoveredFiles
+	return discoveredFiles
 }
 
 func initCommandsFile() string {
-  path, _ := os.Getwd()
-  targetFile := path + "/commands.yaml"
+	path, _ := os.Getwd()
+	targetFile := path + "/commands.yaml"
 
-  _, err := os.Stat(targetFile)
-  if err == nil {
-    log.Fatalf("commands.yaml file already exists at " + path)
-    panic("commands.yaml file already exists at " + path)
-  }
+	_, err := os.Stat(targetFile)
+	if err == nil {
+		log.Fatalf("commands.yaml file already exists at " + path)
+		panic("commands.yaml file already exists at " + path)
+	}
 
-  commandMap := make(map[string][]command)
-  commandMap["example"] = []command{
-    command{Command: "echo this is an example command"},
-  }
-  t := configFile{
-    Shell:    "/bin/bash",
-    Commands: commandMap,
-  }
-  content, _ := yaml.Marshal(&t)
-  err = ioutil.WriteFile(targetFile, []byte(content), 0644)
-  if err != nil {
-    log.Fatalf("Unable to create file " + path)
-  }
+	commandMap := make(map[string][]command)
+	commandMap["example"] = []command{
+		command{Command: "echo this is an example command"},
+	}
+	t := configFile{
+		Shell:    "/bin/bash",
+		Commands: commandMap,
+	}
+	content, _ := yaml.Marshal(&t)
+	err = ioutil.WriteFile(targetFile, []byte(content), 0644)
+	if err != nil {
+		log.Fatalf("Unable to create file " + path)
+	}
 
-  return path
+	return path
 }
 
 func (c *configFile) getConf() *configFile {
-  files := findCommandFiles()
-  for k := range files {
-    yamlFile, err := ioutil.ReadFile(files[k])
-    if err != nil {
-      log.Printf("yamlFile.Get err   #%v ", err)
-    }
-    tempStruct := configFile{}
-    err = yaml.Unmarshal(yamlFile, &tempStruct)
-    if err != nil {
-      log.Fatalf("Unmarshal: %v", err)
-    }
+	files := findCommandFiles()
+	for k := range files {
+		yamlFile, err := ioutil.ReadFile(files[k])
+		if err != nil {
+			log.Printf("yamlFile.Get err   #%v ", err)
+		}
+		tempStruct := configFile{}
+		err = yaml.Unmarshal(yamlFile, &tempStruct)
+		if err != nil {
+			log.Fatalf("Unmarshal: %v", err)
+		}
 
-    mergo.Merge(c, tempStruct)
-  }
+		mergo.Merge(c, tempStruct)
+	}
 
-  return c
+	return c
 }
 
 func (c *configFile) getAvailableCommands() {
-  fmt.Println("Available commands:")
-  fmt.Println("\tinit - create a commands.yaml file in the current directory")
-  for k := range c.Commands {
-    fmt.Println("\t" + k)
-  }
+	fmt.Println("Available commands:")
+	fmt.Println("\tinit - create a commands.yaml file in the current directory")
+	for k := range c.Commands {
+		fmt.Println("\t" + k)
+	}
 }
 
 func main() {
-  var dryRun bool
-  flag.BoolVar(&dryRun, "dry", false, "Run in dry mode to print commands")
-  flag.Parse()
+	var dryRun bool
+	flag.BoolVar(&dryRun, "dry", false, "Run in dry mode to print commands")
+	flag.Parse()
 
-  var c configFile
-  c.getConf()
-  args := os.Args[1:]
-  if 0 == len(args) {
-    c.getAvailableCommands()
-    os.Exit(0)
-  }
+	var c configFile
+	c.getConf()
+	args := os.Args[1:]
+	if 0 == len(args) {
+		c.getAvailableCommands()
+		os.Exit(0)
+	}
 
-  commandName := flag.Args()[0]
+	commandName := flag.Args()[0]
 
-  if commandName == "init" {
-    path := initCommandsFile()
-    fmt.Printf("Created commands file at " + path)
-  }
+	if commandName == "init" {
+		path := initCommandsFile()
+		fmt.Printf("Created commands file at " + path)
+	}
 
-  for key := range c.Commands[commandName] {
-    if dryRun == true {
-      fmt.Printf(">\t%s\n", c.Commands[commandName][key].Command)
-    } else {
-      command := c.Commands[commandName][key].Command
-      env := c.Commands[commandName][key].Environment
-      cmd := exec.Command("bash", "-c", command)
-      cmd.Env = append(os.Environ(), envBuilder(env)...)
-      out, err := cmd.CombinedOutput()
-      if err != nil {
-        fmt.Printf("%s", string(out))
-        log.Fatalf("Command failed with %s\n", err)
-      }
-      fmt.Printf("%s", string(out))
-    }
-  }
+	for key := range c.Commands[commandName] {
+		if dryRun == true {
+			fmt.Printf(">\t%s\n", c.Commands[commandName][key].Command)
+		} else {
+			command := c.Commands[commandName][key].Command
+			env := c.Commands[commandName][key].Environment
+			cmd := exec.Command("bash", "-c", command)
+			cmd.Env = append(os.Environ(), envBuilder(env)...)
+			out, err := cmd.CombinedOutput()
+			if err != nil {
+				fmt.Printf("%s", string(out))
+				log.Fatalf("Command failed with %s\n", err)
+			}
+			fmt.Printf("%s", string(out))
+		}
+	}
 }
 
 func envBuilder(env []environment) []string {
-  var result []string
-  for _, row := range env {
-    result = append(result, fmt.Sprintf("%s=%s\n", row.Name, row.Value))
-  }
+	var result []string
+	for _, row := range env {
+		result = append(result, fmt.Sprintf("%s=%s\n", row.Name, row.Value))
+	}
 
-  return result
+	return result
 }
